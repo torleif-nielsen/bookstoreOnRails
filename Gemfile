a945b70c97c6adb6a59138038eb912dcdb2489f0
torleif-nielsen <torleifnielsen@Torleifs-iMac.local>
2015-12-10 19:12:50 -1000

    Added storefront, added img assets

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'therubyracer', platforms: :ruby
 gem 'bootstrap-sass'
 # Install font-awesome library
 gem 'font-awesome-rails'
-
+gem 'simple_form'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211033221) do
+ActiveRecord::Schema.define(version: 20151211041708) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151211033221) do
     t.string   "category"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image_url"
   end
 
 end
